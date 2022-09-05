148b475caf7862da3edcb5027a8473d076bb8ee7
Paulo Henrique Moro dos Santos <paulo.morolol@gmail.com>
2022-09-05 15:08:48 -0300

    feat: created post model.

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60, null=False)
+    content = models.TextField(null=False)
+    is_editable = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="posts"
+    )
+    post_collab = models.ManyToManyField(
+        "users.User", related_name="collab_posts"
+    )
+    post_likes = models.ManyToManyField(
+        "users.User", related_name="liked_posts"
+    )
