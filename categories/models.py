cff3efc664a1a0e2267b1d02cc088ab718fc6d53
elefantinhos2 <douglasramires01@gmail.com>
2022-09-06 14:48:34 -0300

    fix: add_unique_name_and_validate_unique_name

--- categories/models.py
+++ categories/models.py
@@ -5,5 +5,5 @@ from django.db import models
 
 class Categories(models.Model):
     id   = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    name = models.CharField(max_length=125)
+    name = models.CharField(max_length=125, unique=True)
 
