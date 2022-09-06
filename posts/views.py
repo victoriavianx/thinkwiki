7034a31a89dfb78688a9757aa2f70157502f84ba
deamelo <deyahmelo@hotmail.com>
2022-09-05 22:26:36 -0300

    feat: comment serializer, url and view-list/create created

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+from django.shortcuts import get_object_or_404
+
+from .models import Comment, Post
+from .serializers import CommentSerializer
+
+
+class CommentView(generics.ListCreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs["id_post"])
+
+        serializer.save(user=self.request.user, post=post)
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs["id_post"])
+
+        return Comment.objects.filter(post=post)
