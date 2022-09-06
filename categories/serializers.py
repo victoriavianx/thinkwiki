0f5b87a5e70fd74b215743eee8e4496c7570f1cf
elefantinhos2 <douglasramires01@gmail.com>
2022-09-05 21:26:07 -0300

    feat: categories_model_created

---
+++ categories/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+
+from categories.models import Categories
+
+
+class CreateCategorieSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Categories
+        fields = [
+            "name",
+        ]
+        read_only_fields = ["post"]
