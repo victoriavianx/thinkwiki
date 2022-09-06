7fbc157b960ded615b9f9246156a6125a287ba17
victoriavianx <victoriagarcia963@gmail.com>
2022-09-06 14:12:08 -0300

    update: post serializer

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
+from posts.models import Post
 from .models import Comment
 
 from users.serializers import UserSerializer
 
+class PostCreateListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "updated_at"]
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    class Meta:
+        depth = 1
+        model = Post
+        fields = "__all__"
+        read_only_fields = ["id", "created_at", "updated_at"]
 
 class CommentSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
