651b083dc8480b23d9514e6e2f18803e69c5eff1
Paulo Henrique Moro dos Santos <paulo.morolol@gmail.com>
2022-09-08 16:09:49 -0300

    fix: fixed return for posts, added insomnia wsp with configs for posts, login, category

--- posts/permissions.py
+++ posts/permissions.py
@@ -2,6 +2,7 @@ from rest_framework import permissions
 
 class PostSafeMethodsPermission(permissions.BasePermission):
     def has_permission(self, request, view):
+        
         if request.method in permissions.SAFE_METHODS:
             return True
         if request.user.is_anonymous == True:
@@ -10,15 +11,16 @@ class PostSafeMethodsPermission(permissions.BasePermission):
 
 class PostEditPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        
-        return request.user == obj.owner | request.user.is_superuser | (request.user in obj.post_collab and request.method == 'PATCH')
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.owner or request.user.is_superuser or (request.user in obj.post_collab.all() and request.method == 'PATCH')
 
 
 class PostCollabAdd(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner | request.user.is_superuser
+        return request.user == obj.owner or request.user.is_superuser
 
 class CollabEditorsListPermission(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return request.user == obj.owner | request.user.is_superuser
+        return request.user == obj.owner or request.user.is_superuser
         
--- posts/urls.py
+++ posts/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.PostCreateListView.as_view()),
-    path('posts/categories/<id_category>/', views.PostCreateListView.as_view()),
-    path('posts/<id_post>/<id_user>/', views.ContribView.as_view()),
-    path('posts/<id_post>/', views.PostRetrieveEditDeleteViews.as_view()),
-    path('posts/users/<id_user>/', views.ListUserPostsView.as_view()),
-    path('posts/<id_post>/comments/', views.CommentView.as_view()),
+    path('posts/categories/<str:id_category>/', views.PostCreateListView.as_view()),
+    path('posts/users/<str:id_user>/', views.ListUserPostsView.as_view()),
     path('posts/like/', views.RetrieveUserLikedPosts.as_view()),
-    path('posts/like/<id_post>/', views.UpdateUserLikePost.as_view()),
+    path('posts/like/<str:id_post>/', views.UpdateUserLikePost.as_view()),
+    path('posts/<str:id_post>/<str:id_user>/', views.ContribView.as_view()),
+    path('posts/<str:id_post>/comments/', views.CommentView.as_view()),
+    path('posts/<str:id_post>/', views.PostRetrieveEditDeleteViews.as_view()),
 ]
