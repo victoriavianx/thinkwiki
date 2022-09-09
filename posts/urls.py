3357425a41bdae4d7f7bc3e21af72fc0037b3656
victoriavianx <victoriagarcia963@gmail.com>
2022-09-09 14:03:02 -0300

    fix: posts and comments urls

--- posts/urls.py
+++ posts/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('posts/users/<str:id_user>/', views.ListUserPostsView.as_view()),
     path('posts/like/', views.RetrieveUserLikedPosts.as_view()),
     path('posts/like/<str:id_post>/', views.UpdateUserLikePost.as_view()),
+    path('posts/<str:id_post>/comments/', views.CommentView.as_view()),
+    path('posts/<str:id_post>/comments/<str:id_comment>/', views.CommentDetailView.as_view()),
     path('posts/<str:id_post>/<str:id_user>/', views.ContribView.as_view()),
-    path('comments/posts/<str:id_post>/', views.CommentView.as_view()),
-    path('comments/<str:id_comment>/posts/<str:id_post>/', views.CommentDetailView.as_view()),
     path('posts/<str:id_post>/', views.PostRetrieveEditDeleteViews.as_view()),
 ]
