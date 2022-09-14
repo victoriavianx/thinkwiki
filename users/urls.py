6093b3522a88e7e17d8ee49e72897d3f0712e7eb
victoriavianx <victoriagarcia963@gmail.com>
2022-09-14 10:32:29 -0300

    fix: users url, categories migrations

--- users/urls.py
+++ users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("users/management/", views.UserManagementView.as_view()),
     path("users/friends/", views.ListFriendsView.as_view(), name="list-friends"),
     path("users/friends/pending/", views.ListPendingRequestView.as_view(), name="list-pending-request"),
     path("users/friends/<str:id_friend>/", views.SendFriendRequestAndDeleteFriendshipView.as_view(), name="send-request"),
@@ -9,6 +10,5 @@ urlpatterns = [
     path("users/", views.UserView.as_view()),
     path("users/login/", views.LoginView.as_view()),
     path("users/<pk>/", views.UserDetailView.as_view()),
-    path("users/management/", views.UserManagementView.as_view()),
     path("users/management/<pk>/", views.UserManagementDetailView.as_view()),
 ]
